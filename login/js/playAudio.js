985bc71018d5e30aa068d4f6c79e2a5be0c10c9e
gabriel.aragao <gabriel.aragao@tecnisys.com.br>
2024-03-18 17:07:20 -0300

    js updated

--- login/js/playAudio.js
+++ login/js/playAudio.js
@@ -1,21 +1,21 @@
-var audio = document.getElementById('myAudio');
-var playLink = document.getElementById('playLink');
+// var audio = document.getElementById('myAudio');
+// var playLink = document.getElementById('playLink');
 
-var observer = new IntersectionObserver(function(entries) {
-   entries.forEach(function(entry) {
-       if (entry.isIntersecting) {
-           audio.play();
-           observer.disconnect(); // Parar de observar após a reprodução
-       }
-   });
-});
+// var observer = new IntersectionObserver(function(entries) {
+//    entries.forEach(function(entry) {
+//        if (entry.isIntersecting) {
+//            audio.play();
+//            observer.disconnect(); // Parar de observar após a reprodução
+//        }
+//    });
+// });
 
-observer.observe(playLink);
+// observer.observe(playLink);
 
-document.addEventListener('DOMContentLoaded', function() {
-    var audio = document.getElementById('myAudio');
-    audio.play();
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var audio = document.getElementById('myAudio');
+//     audio.play();
+// });
 
 document.window('load', function() {
     var audio = document.getElementById('myAudio');
