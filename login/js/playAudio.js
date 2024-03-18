7846265a2f5f7ed621258fcb042fbe89da4dc339
gabriel.aragao <gabriel.aragao@tecnisys.com.br>
2024-03-18 16:58:19 -0300

    play audio js updated

--- login/js/playAudio.js
+++ login/js/playAudio.js
@@ -1,4 +1,28 @@
+var audio = document.getElementById('myAudio');
+var playLink = document.getElementById('playLink');
+
+var observer = new IntersectionObserver(function(entries) {
+   entries.forEach(function(entry) {
+       if (entry.isIntersecting) {
+           audio.play();
+           observer.disconnect(); // Parar de observar após a reprodução
+       }
+   });
+});
+
+observer.observe(playLink);
+
 document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('myAudio');
+    audio.play();
+});
+
+document.window('load', function() {
+    var audio = document.getElementById('myAudio');
+    audio.play();
+});
+
+document.addEventListener('click', function() {
     var audio = document.getElementById('myAudio');
     audio.play();
 });
